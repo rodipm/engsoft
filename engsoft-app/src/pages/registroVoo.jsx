7fe4eede122cd730d0d4dcf7c6181daf2bd601d5
GZucchi <gabriel.zucchi.souza@usp.br>
2019-05-17 12:55:18 -0300

    primeira_versão
    Primeira versão da página de registro de voo

--- engsoft-app/src/pages/registroVoo.jsx
+++ engsoft-app/src/pages/registroVoo.jsx
@@ -6,11 +6,65 @@ export class RegistroVoo extends React.Component{
     render (){
         return (
             <div>
-                <div className='azul'>
-                    <h1>
-                        REGISTRO DE VOO
-                    </h1>
-                </div>
+                <form className="fundo">
+                
+                    <h2 className="titulo">Registrar Voo</h2>
+                
+                    <div htmlFor="nMatricula" className="grey-text">
+                        <div className="texto-nMatricula">
+                            Número de Matricula
+                        </div>
+
+                        <input type="text" className="form-control"/>
+
+                        <div class="button">
+                            <button color="unique" type="submit" class="botao">
+                                Buscar
+                            </button>
+                        </div>
+                    </div>                    
+
+                    <div>
+                        <p>Nome:</p>
+                        <p></p>
+                    </div>
+                    
+                    <br />
+
+                    <div htmlFor="horaVoo" className="grey-text">
+                        <div className="texto-horaVoo">
+                            Hora do Voo
+                        </div>
+                        <input type="text" className="form-control"/>
+                    </div>
+
+                    <br />
+
+                    <div htmlFor="nHorasDoVoo" className="grey-text">
+                        <div className="texto-nHorasDoVoo">
+                            Número de Horas do Voo
+                        </div>
+                        <input type="text" className="form-control"/>
+                    </div>
+
+                    <br />
+                    
+                    <div htmlFor="parecer" className="grey-text">
+                        <div className="texto-parecer">
+                            Parecer
+                        </div>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    
+                    <br />
+                    
+                    <div class="button">
+                        <button color="unique" type="submit" class="botao">
+                            Matricular
+                        </button>
+                    </div>
+
+                </form>
             </div>
         )
     }
