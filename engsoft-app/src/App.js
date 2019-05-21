856350722a1117f0bc17f7318644f78c769ae750
Caio Teixeira <tex.caio@gmail.com>
2019-05-21 10:10:19 -0300

    added homepage and fixed cadastro

--- engsoft-app/src/App.js
+++ engsoft-app/src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { RegistroVoo } from './pages/registroVoo';
 import {Cadastro} from './pages/cadastro';
+import { Homepage } from './pages/homepage';
 
 class App extends Component {
   render(){
       return (
         <div className="App">
           <Cadastro />
-          
+
         </div>
       );
     }
