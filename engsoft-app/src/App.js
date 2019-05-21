fbf01ac978df34eb551cffd2230dfebcc9593177
Caio Teixeira <tex.caio@gmail.com>
2019-05-21 10:56:09 -0300

    add navbar component

--- engsoft-app/src/App.js
+++ engsoft-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { RegistroVoo } from './pages/registroVoo';
-import {Cadastro} from './pages/cadastro';
+import { Cadastro } from './pages/cadastro';
 import { Homepage } from './pages/homepage';
 
 class App extends Component {
