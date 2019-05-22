53061029cec9ce5768c1e77bc580a0f558e16174
IanRosa <ian.rosa@thunderatz.org>
2019-05-22 00:56:46 -0300

    testes variados
    .

--- back/app.py
+++ back/app.py
@@ -1,17 +1,25 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask import request, redirect, render_template, url_for
 
 # project imports
 from escola.resources.aluno_resource import AlunoResource, AlunosResource, HorasVooAlunoResource
 from escola.resources.aula_resource import AulaResource, AulasResource
 
+# set template directory to ...>engsoft-app>src>pages
+template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+template_dir = os.path.join(template_dir, 'engsoft-app')
+template_dir = os.path.join(template_dir, 'src')
+template_dir = os.path.join(template_dir, 'pages')
+
 # create flask app
 app = Flask(__name__)
 
 # configure db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco_projeto_engsoft.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///banco_projeto_engsoft.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SQLALCHEMY_ECHO'] = True
@@ -34,6 +42,17 @@ api.add_resource(AlunosResource, '/alunos')
 api.add_resource(HorasVooAlunoResource, '/aluno/horas_voo')
 api.add_resource(AulaResource, '/aula')
 api.add_resource(AulasResource, '/aulas')
+
+# routines for each route
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+@app.route('/post_aluno', methods = ['POST'])
+def post_aluno():
+    AlunoResource.post( request.form['nome'], request.form['CPF'])
+    return redirect(url_for('index'))
+
 
 # main
 if __name__ == '__main__':
--- engsoft-app/src/pages/cadastro.jsx
+++ engsoft-app/src/pages/cadastro.jsx
@@ -7,42 +7,42 @@ export class Cadastro extends React.Component {
         return (
             <div>
                 <Navbar />
-                <form className="fundo">
+                <form className="fundo" action="/post_aluno">
                     <h2 className="titulo">Matricular Aluno</h2>
                     <div id="formulario">
                         <div htmlFor="nome" className="grey-text">
                             <div className="texto-nome">
                                 Nome
                             </div>
-                        <input type="text" className="form-control col-3"/>
+                        <input type="text" name = "nome"className="form-control col-3"/>
                         </div>
                         <br />
                         <div htmlFor="cpf" className="grey-text">
                             <div className="texto-cpf">
                                 CPF
                             </div>
-                        <input type="text" className="form-control col-3"/>
+                        <input type="text" name = "CPF" className="form-control col-3"/>
                         </div>
                         <br />
                         <div htmlFor="rg" className="grey-text">
                             <div className="texto-rg">
                                 RG
                             </div>
-                        <input type="text" className="form-control col-3"/>
+                        <input type="text" name = "RG" className="form-control col-3"/>
                         </div>
                         <br />
                         <div htmlFor="endereco" className="grey-text">
                             <div className="texto-endereco">
                                 Endereço
                             </div>
-                        <input type="text" className="form-control col-3"/>
+                        <input type="text" name = "endereco" className="form-control col-3"/>
                         </div>
                         <br />
                         <div htmlFor="idade" className="grey-text">
                             <div className="texto-idade">
                                 Idade
                             </div>
-                        <input type="text" className="form-control col-3"/>
+                        <input type="text" name = "idade" className="form-control col-3"/>
                         </div>
                         <br />
                         <div class="button">
