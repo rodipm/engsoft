9884a050d8efcc24cb17598fea7752d6c1e84a76
Caio Teixeira <tex.caio@gmail.com>
2019-05-22 09:53:54 -0300

    add fonts

--- engsoft-app/src/pages/cadastro.jsx
+++ engsoft-app/src/pages/cadastro.jsx
@@ -33,47 +33,45 @@ export class Cadastro extends React.Component {
                 <Navbar />
                 <form className="fundo" onSubmit={this.handleSubmit}>
                     <h2 className="titulo">Matricular Aluno</h2>
-                    <div id="formulario">
+                    <div id="formulario" className="container">
                         <div htmlFor="nome" className="grey-text">
-                            <div className="texto-nome">
+                            <div className="label-form">
                                 Nome
                             </div>
                             <input type="text" name="nome" value={this.state.nome} onChange={this.handleChange} className="form-control col-3" />
                         </div>
                         <br />
                         <div htmlFor="cpf" className="grey-text">
-                            <div className="texto-cpf">
+                            <div className="label-form">
                                 CPF
                             </div>
                             <input type="text" name="CPF" value={this.state.CPF} onChange={this.handleChange} className="form-control col-3" />
                         </div>
                         <br />
                         <div htmlFor="rg" className="grey-text">
-                            <div className="texto-rg">
+                            <div className="label-form">
                                 RG
                             </div>
                             <input type="text" name="RG" value={this.state.RG} onChange={this.handleChange} className="form-control col-3" />
                         </div>
                         <br />
                         <div htmlFor="endereco" className="grey-text">
-                            <div className="texto-endereco">
+                            <div className="label-form">
                                 Endereço
                             </div>
                             <input type="text" name="endereco" value={this.state.endereco} onChange={this.handleChange} className="form-control col-3" />
                         </div>
                         <br />
                         <div htmlFor="idade" className="grey-text">
-                            <div className="texto-idade">
+                            <div className="label-form">
                                 Idade
                             </div>
                             <input type="text" name="idade" value={this.state.idade} onChange={this.handleChange} className="form-control col-3" />
                         </div>
                         <br />
                         <div class="button">
-                            <button color="unique" type="submit" class="botao">
-                                Matricular
-                            </button>
-                        </div>
+                        <a href="/registroVoo" target="_blank"><button type="submit" className="button-primary" onclick="alert(); ">Matricular</button></a>
+                        </div>  
                     </div>
                 </form>
             </div>
